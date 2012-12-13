19e8b1cc128618a1e34e118c71ac7eabe88bf8af
Tom Davies <atomgiant@gmail.com>
2012-12-13 09:50:32 -0500

    Add alias for redis

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -29,6 +29,9 @@ alias bi='bundle install --path vendor'
 alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
+# Redis
+alias redis-start='redis-server /usr/local/etc/redis.conf'
+
 # Misc
 alias listening_ports='lsof -i -n -P | grep LISTEN'   # Show which processes are using which ports
 
