88b4f6aa38eb1c12d2ff5ecfefbb170f22f1b40f
Tom Davies <atomgiant@gmail.com>
2024-02-07 13:34:01 -0500

    Disable HOMEBREW auto update.
    Add docker/bin to path.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,9 @@
 # add libpq for pg gem
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 
+# add docker
+export PATH="/users/tdavies/.docker/bin:$PATH"
+
 # Our list of directories we can cd to from anywhere
 export CDPATH=.:~/code
 
@@ -17,5 +20,8 @@ export GREP_COLOR='3;33'
 export GIT_EDITOR="vim"
 export EDITOR='vim'
 export THOR_MERGE='gvimdiff'
+
+# Never update homebrew packages automatically
+export HOMEBREW_NO_AUTO_UPDATE="1"
 
 ulimit -n 2048
